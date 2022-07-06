bc63f1ba5b3b50297891572eabdc98970145cd88
Parthib <parthibdutta02@gmail.com>
2022-07-06 09:04:09 +0530

    Added few files

---
+++ c/variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int a=10;
+void fun(){
+    int b=20;
+    static int c=30;
+    a++;
+    b++;
+    c++;
+    printf("Global:%d\n Local:%d\n Static:%d\n",a,b,c);
+}
+
+int main(){
+    int d=40;
+    printf("Auto: %d\n",d);
+    fun();
+    fun();
+    fun();
+    return 0;
+}
